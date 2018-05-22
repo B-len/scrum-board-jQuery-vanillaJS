9e7a5769d6e74239f249efe112e49ddd6e46f3ee
Juan <jupegarnica@gmail.com>
2018-05-22 12:33:56 +0200

    api/lists reads a file and returns it

--- backend/getAllBoard.js
+++ backend/getAllBoard.js
@@ -1,5 +1,18 @@
-let getAllboard = (req, res) => {
-    // read board.json from file system and send it back into the response as JSON
-    res.send('hola funciono!')
+let fs = require('fs');
+let getBoard = (req, res) => {
+    // read board.json from fs and parse it to JS object and send it back as JSON
+    fs.readFile('backend/board.json', 'utf-8', (error, file) => {
+        if(error) {
+            res.status(500).send('problem reading  the file');
+            return;
+        } else {
+            try {
+                fileObj = JSON.parse(file);
+                res.json(fileObj)
+            } catch (error) {
+                res.status(500).send('problem parsing the file ');
+            }
+        }
+    })
 }
-module.exports = getAllboard;
+module.exports = getBoard;
